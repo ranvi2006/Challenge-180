3471b3aa55342b27916b13e9f02568da6c521cbf
ranvi2006 <rajurk2002@gmail.com>
2025-09-15 08:35:50 +0530

    Question 2 done

---
+++ Question1.java
@@ -0,0 +1,15 @@
+//Declare an int, double, and String variable with values, and print them.
+public class Question1
+{
+public static void main(String args[])
+{
+    int age=20;
+    double marks=279.65;
+    String name="Raju Kumar Mandal";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(marks);
+}
+
+}
---
+++ Question2.java
@@ -0,0 +1,16 @@
+//Swap two integers using a temporary variable.
+public class Question2
+{
+public static void main(String args[])
+{
+   int a=10;
+   int b=20;
+   int tem=a;
+   a=b;
+   b=tem;
+
+   System.out.println(a);
+   System.out.println(b);
+}
+
+}

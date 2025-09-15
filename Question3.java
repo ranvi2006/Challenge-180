6d93e4ae454269f93ba8ce641382c1fd4bc34053
ranvi2006 <rajurk2002@gmail.com>
2025-09-15 13:45:30 +0530

    question 3 done

---
+++ Question3.java
@@ -0,0 +1,11 @@
+public class Question3 {
+    
+    public static void main(String args[])
+    {
+       double dub=1.6693269;
+       int intVal=(int)dub;
+       System.out.println(dub);
+       System.out.println(intVal);
+
+    }
+}
